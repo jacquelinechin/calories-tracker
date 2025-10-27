8b054eea0a73b0b0b0f17ca4f9259d85907efa6b
Jacqueline Chin <jacqueline@dyn-edge.com>
2025-10-27 23:46:01 +0800

    Update Home

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -6,16 +6,9 @@ namespace CaloriesTracker.Pages
 {
     public partial class Home
     {
-        //[Inject]
-        //public required Blazored.LocalStorage.ILocalStorageService localStorage { get; set; }
-        //[Inject]
-        //public required Supabase.Client SupabaseClient { get; set; }
-        //[Inject]
-        //public required AuthService AuthService { get; set; }
         [Inject]
         public required MealService MealService { get; set; }
 
-        //private Supabase.Gotrue.User? user;
         private List<Meal> meals = new();
         private Meal inputMeal = new();
         private int totalCalories;
@@ -23,13 +16,8 @@ namespace CaloriesTracker.Pages
 
         protected override async Task OnInitializedAsync()
         {
-            //data = await localStorage.GetItemAsync<List<MealItem>>(DateTime.Today.ToString("dd-MM-yyyy")) ?? new List<MealItem>();
-            //dailyCaloriesGoal = await localStorage.GetItemAsync<int?>("dailyCaloriesGoal") ?? 0;
-
-            //user = await AuthService.GetCurrentUserAsync();
-
-            //var response = await SupabaseClient.From<Meal>().Get();
-            //meals = response.Models;
+            inputMeal = new();
+            totalCalories =  0;
 
             meals = await MealService.GetMealsAsync();
 
@@ -41,25 +29,6 @@ namespace CaloriesTracker.Pages
 
         private async Task UpsertMeal(Meal? meal = null)
         {
-            //data.Add(newMeal);
-            //await localStorage.SetItemAsync(DateTime.Today.ToString("dd-MM-yyyy"), data);
-
-            //newItem = new();
-            //totalCalories += newItem.Calories;
-
-            //var newMeal = new Meal
-            //{
-            //    UserId = Guid.Parse(user?.Id),
-            //    Date = DateTime.UtcNow.Date,
-            //    MealName = "Example Meal",
-            //    MealType = MealType.Breakfast,
-            //    Calories = 400,
-            //    Fullness = Fullness.Full
-            //};
-
-            //await SupabaseClient.From<Meal>().Insert(newMeal);
-            //meals.Add(newMeal);
-
             var mealToUpsert = new Meal();
 
             if (meal == null)
@@ -80,20 +49,13 @@ namespace CaloriesTracker.Pages
             }
 
             await MealService.UpsertMealAsync(mealToUpsert);
-            meals = await MealService.GetMealsAsync();
-            inputMeal = new();
+            await OnInitializedAsync();
         }
 
         private async Task DeleteMeal(Guid mealId)
         {
             await MealService.DeleteMealAsync(mealId);
-
-            meals = await MealService.GetMealsAsync();
-            totalCalories = 0;
-            foreach (var item in meals)
-            {
-                totalCalories += item.Calories;
-            }
+            await OnInitializedAsync();
         }
 
         private void SetFullness(Fullness level)
